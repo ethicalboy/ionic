5fe70011f3b044bd0b3c215384b28d182055a74e
Vikas Chauhan <chauhanvikas710@gmail.com>
2017-08-09 21:45:12 +0530

    Update global.ts

--- Expense App V1/src/providers/global.ts
+++ Expense App V1/src/providers/global.ts
@@ -1,7 +1,4 @@
 export var link = {
-	url : "http://www.vconnect.ml/api/",
-	scan_url : "http://www.vconnect.ml/",
-	host : "http://www.vconnect.ml"
 /*	url : "http://localhost/xshare/api/",
 	scan_url : "http://localhost/xshare/",
 	host : "http://localhost/"*/
